fbd1d01604688df023c20c84a7cb59a828f6f3fc
Ming Li <ming_li@jordans.co.uk>
2018-02-21 11:38:18 +0000

    Logging

--- src/main/java/com/vistra/notification/NotificationController.java
+++ src/main/java/com/vistra/notification/NotificationController.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.stream.Collectors;
 
 /**
  * @author Ming Li
@@ -18,22 +20,25 @@ public class NotificationController {
 
     @GetMapping
     @RequestMapping("/")
-    public String home(final HttpServletRequest request, final HttpServletResponse response){
+    public String home(final HttpServletRequest request, final HttpServletResponse response) throws IOException {
         log.info("HOME");
+        log.info("Request: " + request.getReader().lines().collect(Collectors.joining(System.lineSeparator())));
         return "Home";
     }
 
     @GetMapping
     @RequestMapping("/test")
-    public String testGet(final HttpServletRequest request, final HttpServletResponse response){
+    public String testGet(final HttpServletRequest request, final HttpServletResponse response) throws IOException {
         log.info("Test");
+        log.info("Request: " + request.getReader().lines().collect(Collectors.joining(System.lineSeparator())));
         return "Test";
     }
 
     @PostMapping
     @RequestMapping("/callback")
-    public String testCallback(final HttpServletRequest request, final HttpServletResponse response){
+    public String testCallback(final HttpServletRequest request, final HttpServletResponse response) throws IOException {
         log.info("Callback");
+        log.info("Request: " + request.getReader().lines().collect(Collectors.joining(System.lineSeparator())));
         return "Callback";
     }
 }
