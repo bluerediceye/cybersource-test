14e4f8a20466ede3ebe638a965b7fcc86425eea9
Ming Li <ming_li@jordans.co.uk>
2018-02-21 10:41:43 +0000

    log4j2

--- src/main/java/com/vistra/notification/NotificationApplication.java
+++ src/main/java/com/vistra/notification/NotificationApplication.java
@@ -1,9 +1,11 @@
 package com.vistra.notification;
 
+import lombok.extern.log4j.Log4j2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Log4j2
 public class NotificationApplication {
 
 	public static void main(String[] args) {
